44f151089956a5bd5fb05674ec54ce82af1bf291
melnichenko.andrey <melnichenko.andrey@gmail.com>
2017-11-25 12:40:56 +0200

    add new clesses

--- src/main/java/center/kit/app/homework/firstclass.java
+++ src/main/java/center/kit/app/homework/firstclass.java
@@ -2,6 +2,6 @@ package center.kit.app.homework;
 
 public class firstclass {
     public static void main (String[] args){
-        System.out.println("Hello world!!!!");
+        //System.out.println("Hello world!!!!");
     }
 }
---
+++ src/main/java/center/kit/runners/classwork/lesson4/WideningCastingRunner.java
@@ -0,0 +1,10 @@
+package center.kit.runners.classwork.lesson4;
+
+import center.kit.app.classwork.lesson4.WideningCasting;
+
+public class WideningCastingRunner {
+    public static void main(String[] args) {
+        WideningCasting wideningCasting = new WideningCasting();
+        wideningCasting.widening();
+    }
+}
