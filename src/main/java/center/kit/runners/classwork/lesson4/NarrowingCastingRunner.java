bf5fb96657b05919db4d09e8964a2ebc895d046a
melnichenko.andrey <melnichenko.andrey@gmail.com>
2017-11-25 13:20:19 +0200

    [jcore] NarrowingCasting

---
+++ src/main/java/center/kit/runners/classwork/lesson4/NarrowingCastingRunner.java
@@ -0,0 +1,10 @@
+package center.kit.runners.classwork.lesson4;
+
+import center.kit.app.classwork.lesson4.NarrowingCasting;
+
+public class NarrowingCastingRunner {
+    public static void main(String[] args) {
+        NarrowingCasting narrowingCasting = new NarrowingCasting();
+        narrowingCasting.narrowing();
+    }
+}
