953efb24600aa799ee94322c9a5e8a74989c59c2
melnichenko.andrey <melnichenko.andrey@gmail.com>
2017-12-09 22:33:59 +0200

    upgrade Reader/Writer

--- src/main/java/center/kit/app/classwork/lesson8/MyFileReader.java
+++ src/main/java/center/kit/app/classwork/lesson8/MyFileReader.java
@@ -5,32 +5,39 @@ import java.io.*;
 public class MyFileReader {
     public static void myreader() {
         String currentDir = System.getProperty("user.dir");
+        String pathSeparator = System.getProperty("path.separator"); // :
+        String fileSeparator = System.getProperty("file.separator"); // /
+        String inputFile = "input.txt", outputFile = "output.txt", osType = System.getProperty("os.name");
+        System.out.println(currentDir+"\n"+pathSeparator+"\n"+fileSeparator+"\n"+osType);
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\git\\javacore\\output.txt"));
-            File file = new File("D:\\git\\javacore\\input.txt");
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(currentDir+fileSeparator+outputFile));
+            File file = new File(currentDir+fileSeparator+inputFile);
             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
             try (bufferedReader) {
                 String line;
                 while ((line = bufferedReader.readLine())!= null) {
+                    System.out.println(line);
                     if (isPalindrom(line)) {
                         bufferedWriter.append(line+" this is PALINDROM");
-                        //bufferedWriter.newLine();
-                        bufferedWriter.close();
+                        bufferedWriter.newLine();
+                        //bufferedWriter.close();
                     } else {
                         bufferedWriter.append(line+" this is Not palindrom");
-                        //bufferedWriter.newLine();
-                        bufferedWriter.close();
+                        bufferedWriter.newLine();
+
                     }
                 }
+                bufferedWriter.close();
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File not Found!!!!");
+            e.printStackTrace();
+            //System.out.println("File not Found!!!!");
         } catch (IOException e) {
-            //e.printStackTrace();
-            System.out.println("java.io.IOException: Stream closed");
-        } finally {
+            e.printStackTrace();
+            //System.out.println("java.io.IOException: Stream closed");
+        } /*finally {
             System.out.println("Regardless about exception you will see this text");
-        }
+        }*/
     }
 
     public static boolean isPalindrom(String value) {
