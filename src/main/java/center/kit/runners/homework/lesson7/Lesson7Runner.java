33ec75e502191e26f612b3b7bd37e4fbc567792c
a.melnychenko <melnichenko.andrey@gmail.com>
2017-12-15 16:57:10 +0200

    add new fix

--- src/main/java/center/kit/runners/homework/lesson7/Lesson7Runner.java
+++ src/main/java/center/kit/runners/homework/lesson7/Lesson7Runner.java
@@ -1,7 +1,9 @@
 package center.kit.runners.homework.lesson7;
 
-public class Lesson7Runner {
-    public static void main(String[] args) {
+import center.kit.app.homework.lesson7.MainMenu;
 
+public class Lesson7Runner {
+    public static void main(String[] args) throws Exception{
+        MainMenu.menu();
     }
 }
