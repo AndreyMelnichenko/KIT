718fba1edc3530b65076f2bed1d642ba8f5adcd9
a.melnychenko <melnichenko.andrey@gmail.com>
2018-01-10 11:31:07 +0200

    add HW8.2

--- src/main/java/center/kit/app/homework/lesson8/ConsoleWriter.java
+++ src/main/java/center/kit/app/homework/lesson8/ConsoleWriter.java
@@ -1,11 +1,45 @@
 package center.kit.app.homework.lesson8;
 
+import java.io.*;
+
 public class ConsoleWriter {
-    public static final String CONS_LINE = "Enter line to write it to file";
-    public static boolean isExit (String val){
-        if (val.equals("*exit*")){
+    static final String CONS_LINE = "Enter line to write(*exit*)_>";
+
+    public static void enterValue(String outputFile) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            File file = new File(outputFile);
+            try {
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+                String inputedVal = "";
+                while (!(isExit(inputedVal))) {
+                    System.out.println(CONS_LINE);
+                    inputedVal = bufferedReader.readLine();
+                    if (inputedVal.equals("")) {
+                        System.out.println("You entered empty value!");
+                    } else if (inputedVal.equals("*exit*")) {
+                        System.out.println("Program is ending...");
+                    } else {
+                        System.out.println(inputedVal);
+                        bufferedWriter.append(inputedVal);
+                        bufferedWriter.newLine();
+                    }
+                }
+                bufferedWriter.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File not Found!!!!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException!");
+        }
+    }
+
+    private static boolean isExit(String val) {
+        if (val.equals("*exit*")) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
--- src/main/java/center/kit/runners/homework/lesson8/Lesson8Runner.java
+++ src/main/java/center/kit/runners/homework/lesson8/Lesson8Runner.java
@@ -1,11 +1,13 @@
 package center.kit.runners.homework.lesson8;
 
+import center.kit.app.homework.lesson8.ConsoleWriter;
 import center.kit.app.homework.lesson8.ReadFile;
 
 public class Lesson8Runner {
     public static void main(String[] args) {
         String inputFile = "input.txt", outputFile = "output.txt";
-        ReadFile.isPalindromCheck(inputFile,outputFile);
-        ReadFile.arraySort(inputFile,outputFile);
+        //ReadFile.isPalindromCheck(inputFile,outputFile);
+        //ReadFile.arraySort(inputFile,outputFile);
+        ConsoleWriter.enterValue(outputFile);
     }
 }
